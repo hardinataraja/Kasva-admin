9de36132f4364da7135a99895b48a3ae0557b269
hardinataraja <hardinataraja@gmail.com>
2026-03-26 01:15:44 +0700

    Update service worker cache version and files

--- sw.js
+++ sw.js
@@ -1,16 +1,40 @@
-const CACHE_NAME = 'kasva-admin-v1';
+// NAIKKAN VERSI CACHE (Misal dari v2 ke v3)
+const CACHE_NAME = 'kasva-admin-v3'; 
+
 const urlsToCache = [
+  
   'index.html',
-  'manifest.json'
+  
+  'manifest.json',
+  
 ];
+
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
+      console.log('PWA: Mencache file baru index.html');
       return cache.addAll(urlsToCache);
     })
   );
 });
+
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('PWA: Menghapus cache lama', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
 
 self.addEventListener('fetch', event => {
   event.respondWith(
